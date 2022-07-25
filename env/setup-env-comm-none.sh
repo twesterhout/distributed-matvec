d48baaf3e4bd1660cec80b37fd5434d33865d7ca
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-07-26 00:32:26 +0200

    Environment for building the exported lib

---
+++ env/setup-env-comm-none.sh
@@ -0,0 +1,11 @@
+export GASNET_BACKTRACE=1
+export CHPL_COMM=none
+export CHPL_LLVM=system
+export CHPL_UNWIND=bundled
+export CHPL_TARGET_CPU=native
+export CHPL_HWLOC=bundled
+export CHPL_RE2=none
+export CHPL_GMP=none
+export CHPL_LIB_PIC=pic
+. "${HOME}/src/chapel/util/setchplenv.bash"
+export LD_LIBRARY_PATH="$PWD/third_party/lib:$LD_LIBRARY_PATH"
