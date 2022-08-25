33efe97ff7ae88158670cb795a77a1c2b02abb6c
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-08-25 17:31:38 +0200

    Switch to OFI

--- env/setup-env-lilo.sh
+++ env/setup-env-lilo.sh
@@ -1,11 +1,15 @@
-. env/chpl-env-lilo.sh
+# . env/chpl-env-lilo.sh
+. env/chpl-env-lilo-ofi.sh
 export LD_LIBRARY_PATH="$PWD/third_party/lib:$LD_LIBRARY_PATH"
 source "${CHPL_HOME}/util/setchplenv.bash"
 # export CHPL_RT_OVERSUBSCRIBED=yes
 # export OMP_NUM_THREADS=1
 export HDF5_USE_FILE_LOCKING=FALSE
 
-example05() {
+run_with_mpi() {
+    APP="$1"
+    shift
+
     case "$1" in
         -nl)
             NUM_LOCALES=$2
@@ -21,5 +25,9 @@ example05() {
             return 1
     esac
     mpirun -np $NUM_LOCALES --bind-to none -mca btl_base_warn_component_unused 0 -- \
-        bin/Example05_real -nl$NUM_LOCALES "$@"
+        "$APP" -nl$NUM_LOCALES "$@"
+}
+
+example05() {
+    run_with_mpi "bin/Example05_real" "$@"
 }
