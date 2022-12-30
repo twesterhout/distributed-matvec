441ec581fc37c246683ad992ea51987a01e1dd41
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-12-30 12:26:48 +0100

    Is CHPL_HOME set in the Docker container?

--- env/setup-env-comm-none.sh
+++ env/setup-env-comm-none.sh
@@ -1,3 +1,6 @@
+if [ -z "${CHPL_HOME}+x" ]; then
+  export CHPL_HOME="$HOME/src/chapel"
+fi
 export GASNET_BACKTRACE=1
 export CHPL_COMM=none
 export CHPL_LLVM=system
@@ -7,5 +10,5 @@ export CHPL_HWLOC=bundled
 export CHPL_RE2=none
 export CHPL_GMP=none
 export CHPL_LIB_PIC=pic
-. "${HOME}/src/chapel/util/setchplenv.bash"
+. "${CHPL_HOME}/util/setchplenv.bash"
 export LD_LIBRARY_PATH="$PWD/third_party/lib:$LD_LIBRARY_PATH"
